31580d268bdd49b06c4728ede602b404bc0b2678
matthewellishanson <lifeasmeh@gmail.com>
2025-07-27 00:41:55 -0500

    minimalizing app.jsx to test tailwind setup

--- nba-dashboard/src/App.jsx
+++ nba-dashboard/src/App.jsx
@@ -1,40 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-// import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="min-h-screen bg-gradient-to-r from-purple-500 to-pink-500 flex items-center justify-center">
-      <h1 className="text-5xl text-white font-bold underline">
-        Tailwind is Working!
+    <div className="min-h-screen bg-black text-white flex items-center justify-center">
+      <h1 className="text-6xl font-bold text-blue-400 underline">
+        Tailwind Is Working!
       </h1>
     </div>
-    <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  );
 }
-
-export default App
