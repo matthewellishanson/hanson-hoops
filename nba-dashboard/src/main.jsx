1784dc27c6cb6bfcae898a974294ff25e084f6b9
matthewellishanson <lifeasmeh@gmail.com>
2025-07-27 00:33:32 -0500

    changing to cjs

--- nba-dashboard/src/main.jsx
+++ nba-dashboard/src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css'; // THIS IS REQUIRED
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css' // ✅ THIS IS REQUIRED
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-);
+)
+
 
