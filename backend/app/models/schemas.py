b72e19b82fa367b9890b2a86f437e50abbe9e416
matthewellishanson <lifeasmeh@gmail.com>
2025-07-27 16:40:16 -0500

    adding PlayerProfileStats class to schemas.py

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -1,9 +1,12 @@
+# app/models/schemas.py
+
 from pydantic import BaseModel
 
-class Player(BaseModel):
-    id: str
-    name: str
-
-class GameStat(BaseModel):
-    game_date: str
-    points: int
+class PlayerProfileStats(BaseModel):
+    points: float
+    rebounds: float
+    assists: float
+    blocks: float
+    steals: float
+    fg_pct: float
+    fg3_pct: float
