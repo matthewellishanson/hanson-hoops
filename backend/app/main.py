1d85dab7582d633a227890c93b0b14eab93bfdd3
matthewellishanson <lifeasmeh@gmail.com>
2025-09-26 22:14:17 -0500

    adding docker file to requirements.txt

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,10 +4,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:8080",
+    "https://matthewellishanson.github.io"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
-    allow_origin_regex=r"http://localhost:\d+",
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
