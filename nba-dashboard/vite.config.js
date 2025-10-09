cc9f59249cda99aed9f91ab963f9b617ee366390
matthewellishanson <lifeasmeh@gmail.com>
2025-10-09 17:15:29 -0500

    fixing routes in vite.config.js for deployment

--- nba-dashboard/vite.config.js
+++ nba-dashboard/vite.config.js
@@ -1,10 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Use subpath on github.io, root elsewhere
-const isPages = process.env.BUILD_TARGET === 'pages';
-
 export default defineConfig({
   plugins: [react()],
-  base: isPages ? '/hanson-hoops/' : '/',
+  base: '/hanson-hoops/',   // always use GH Pages subfolder
 })
+
